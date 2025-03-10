9211dbc3154ff64074f7f4afe83cca7a4c2a3b68
samidollars <muss.junior@gmail.com>
2025-03-09 23:33:30 -0500

    Dice Rolling simulator project: Complete the dice face and user input instance

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,32 +1,48 @@
 // Create an app that generate random number.
-
-import java.util.Random;
+import  java.util.*;
+//import java.util.Random;
 
 public class Simulator {
 
     public static void main(String[] args) {
 
-        // Create an instance for this class
-        Random rand = new Random();
+        // Create an instance to allow user to enter a number of roll they would like.
+        Scanner scanner = new Scanner(System.in);
 
-        int rolledNumber = rand.nextInt(6) + 1;
-        System.out.println(display(rolledNumber));
+        try {
+            System.out.println("How many dice would you like to roll? ");
+            int numberOfDice = scanner.nextInt();
+
+            System.out.println("About to roll " + numberOfDice + " dice");
+
+            Random rand = new Random();
+
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                int rolledNumber = rand.nextInt(6) + 1;
+                System.out.println(display(rolledNumber));
+            }
+
+        } catch (InputMismatchException e)
+        {
+            System.out.println("Invalid input! Please enter a number between 1 and 10");
+        }
 
     }
 
     // Create a new method which take an int and returns a string
     static String display(int value)
     {
-        switch (value)
+        return switch (value)
         {
-            case 1: return "---------\n|       |\n|   o   |\n|       |\n---------";
-            case 2: return "---------\n| o     |\n|       |\n|     o |\n---------";
-            case 3: return "---------\n| o     |\n|   o   |\n|     o |\n---------";
-            case 4: return "---------\n| o   o |\n|       |\n| o   o |\n---------";
-            case 5: return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
-            case 6: return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
-            default: return "Not a valid die value";
-        }
+            case 1: yield "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2: yield "---------\n| o     |\n|       |\n|     o |\n---------";
+            case 3: yield "---------\n| o     |\n|   o   |\n|     o |\n---------";
+            case 4: yield "---------\n| o   o |\n|       |\n| o   o |\n---------";
+            case 5: yield "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
+            case 6: yield "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+            default: yield "Not a valid die value";
+        };
 
     }
 }
